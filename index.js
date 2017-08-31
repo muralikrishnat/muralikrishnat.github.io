768403a354a1e6ef802f136540fe6737b1d0b9de
Muralikrishna T <muralikrishna8811@gmail.com>
2017-08-31 14:35:14 +0530

    incresed size of cancas

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-// require('./fe-server')({ fePort: 3435, folder: 'mhddc-render'  });
-require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
+require('./fe-server')({ fePort: 3435, folder: 'paperjs'  });
+// require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
