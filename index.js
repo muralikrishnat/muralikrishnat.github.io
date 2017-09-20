73fccbefcc744794e2d270ec69dcda4ef01aa361
Muralikrishna T <muralikrishna8811@gmail.com>
2017-09-21 02:10:55 +0530

    Added pagination component

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-require('./fe-server')({ fePort: 3435, folder: 'Solr'  });
+require('./fe-server')({ fePort: 3435, folder: 'Solr' });
 // require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
