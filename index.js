bdc84cffdcbe0c5179375d62f2add049738d2276
Muralikrishna T <mtottimpudi@evoketechnologies.com>
2018-02-19 21:33:24 +0530

    Added iot usecase page

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-require('./fe-server')({ fePort: 3435, folder: 'solr-v2' });
+require('./fe-server')({ fePort: 3435, folder: 'gigamesh-iot' });
 // require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
