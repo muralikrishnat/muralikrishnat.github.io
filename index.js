03358a3887deddbe576471fb6763091253f68ed4
Muralikrishna T <mtottimpudi@evoketechnologies.com>
2018-02-28 18:17:43 +0530

    Added temlate list

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-require('./fe-server')({ fePort: 3435, folder: 'gigamesh-iot' });
+// require('./fe-server')({ fePort: 3435, folder: 'gigamesh-iot' });
+require('./fe-server')({ fePort: 3435, folder: 'Solr' });
 // require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
