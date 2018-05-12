acf8b75f4c8a388406fe3e485eae2d15ccaa47a4
Muralikrishna T <mtottimpudi@evoketechnologies.com>
2018-05-13 00:10:15 +0530

    Added meta tags

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // require('./fe-server')({ fePort: 3435, folder: 'gigamesh-iot' });
-require('./fe-server')({ fePort: 3435, folder: 'iot2' });
+require('./fe-server')({ fePort: 3435, folder: 'code-contest' });
 // require('./fe-server')({ fePort: 3435, folder: 'mhddc-render', ip: '10.42.0.1'  });
